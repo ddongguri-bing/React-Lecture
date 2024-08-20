cfdb0b23fc4947e69d86571b2beed52a55f21cd8
ddongguri-bing <ckdalssnsk@naver.com>
2024-08-20 15:47:26 +0900

    Section 01 - Chapter 05

---
+++ section01/chapter05.js
@@ -0,0 +1,32 @@
+// 1. Number Type
+let num1 = 25;
+let num2 = 1.5;
+let num3 = -20;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+let myName = '이수빈';
+let myLocation = '충무로';
+let introduce = myName + myLocation;
+
+// 템플릿 리터럴 문법
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+
+// 3. Boolean Type
+let isSwitch = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Type
